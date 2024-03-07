da7db35b450969240f70aa5368bb2fec8077ab83
Daniel Krigel <daniel.krigel@gm.com>
2024-03-07 15:01:41 +0200

    Set lsp config to not install lua

--- lua/zero-lsp.lua
+++ lua/zero-lsp.lua
@@ -8,12 +8,16 @@ end)
 
 require("mason").setup()
 require('mason-lspconfig').setup({
-  ensure_installed = {'clangd', 'lua_ls', 'rust_analyzer'},
+  ensure_installed = {
+      'clangd',
+      'lua_ls',
+      'rust_analyzer',
+  },
   handlers = {
     lsp_zero.default_setup,
   }
 })
--- require'lspconfig'.clangd.setup{}
--- require'lspconfig'.cmake.setup{}
--- require'lspconfig'.rust_analyzer.setup{}
--- require'lspconfig'.lua_language_server.setup{}
+require'lspconfig'.clangd.setup{}
+require'lspconfig'.cmake.setup{}
+require'lspconfig'.rust_analyzer.setup{}
+--require'lspconfig'.lua_language_server.setup{}
